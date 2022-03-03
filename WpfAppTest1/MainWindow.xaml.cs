2f1fab49ecc2e5d59da3a2c7a6f7bc3f6904730e
maksimmer348 <maksimmer348@gmail.com>
2022-03-03 16:48:11 +0300

    add

---
+++ WpfAppTest1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppTest1;
+/// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+         Button btn = new Button();
+         btn.Width = 10;
+         btn.Height = 10;
+         btn.Content = "Button1";
+         btn.HorizontalAlignment = HorizontalAlignment.Left;
+         btn.Background = (Brush)TypeDescriptor
+             .GetConverter(typeof(Brush)).ConvertFromInvariantString("Red");
+             //new SolidColorBrush(Colors.Red);
+         layoutGrid.Children.Add(btn);
+        }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            string text = tBoxName.Text;
+
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
+        }
+    }
+
+
---
+++ WpfAppTest1/Phone.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfAppTest1;
+
+public class Phone
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+      
+    public override string ToString()
+    {
+        return $"Смартфон {this.Name}; цена: {this.Price}";
+    }
+}
